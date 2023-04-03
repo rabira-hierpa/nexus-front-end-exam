717796490cc92316526ddf6461960be41ad8dd98
rabra <rabira.hierpa@gmail.com>
2023-04-03 19:09:14 +0300

    ✨ feat: added cards together with their style and toggle switch

---
+++ src/utils/helpers/create-cards.ts
@@ -0,0 +1,46 @@
+import { ICard } from "../../components/card/card";
+
+export const cards: ICard[] = [
+  {
+    id: 1,
+    title: "Sapling",
+    description: "HR Management",
+    icon: "../assets/icons/sapling.png",
+    active: true,
+  },
+  {
+    id: 2,
+    title: "Workday",
+    description: "HR Management",
+    icon: "../assets/icons/workday.png",
+    active: false,
+  },
+  {
+    id: 3,
+    title: "Xero",
+    description: "Employers Base",
+    icon: "../assets/icons/xero.png",
+    active: false,
+  },
+  {
+    id: 4,
+    title: "Rippling",
+    description: "Salary Management",
+    icon: "../assets/icons/rippling.png",
+    active: false,
+  },
+  {
+    id: 5,
+    title: "Expensify",
+    description: "HR Management",
+    icon: "../assets/icons/expensify.png",
+    active: false,
+  },
+  {
+    id: 6,
+    title: "Zenefits",
+    description: "HR Management",
+    icon: "../assets/icons/zenefits.png",
+    active: false,
+  },
+];
